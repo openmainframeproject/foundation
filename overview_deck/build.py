f730fdbc5c346554d60ee2c12562c6347d422955
John Mertic <jmertic@linuxfoundation.org>
2021-04-02 08:38:16 -0400

    Update decks for April 2021
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

---
+++ overview_deck/build.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+# The ID of a sample document.
+documents = [
+        {'id':'1Zk3REYGnE6mYIXl52QxzNbVW7XM2NPs_1w7YxscRXrk','mimeType':'application/pdf','filename':'open_mainframe_project_overview_deck.pdf'},
+        {'id':'1Zk3REYGnE6mYIXl52QxzNbVW7XM2NPs_1w7YxscRXrk','mimeType':'application/vnd.openxmlformats-officedocument.presentationml.presentation','filename':'open_mainframe_project_overview_deck.pptx'},
+        {'id':'1KaPgj8AKudkfS7t2TvOxvwWpOb5uu-GP3YwA14su_vY','mimeType':'application/pdf','filename':'open_mainframe_project_host_project.pdf'},
+        {'id':'1KaPgj8AKudkfS7t2TvOxvwWpOb5uu-GP3YwA14su_vY','mimeType':'application/vnd.openxmlformats-officedocument.presentationml.presentation','filename':'open_mainframe_project_host_project.pptx'},
+        {'id':'1SW-JbXTQgwb_OczGeyo5jqc6GmK5CLmBTzg0pb0vMXs','mimeType':'application/pdf','filename':'Open Mainframe Project - Membership Overview.pdf'}
+        ]
+
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+service = build('drive', 'v3', credentials=creds)
+
+# Retrieve the documents contents from the Docs service.
+for document in documents:
+    print("Getting file {}...".format(document['filename']))
+    try:
+        contents = service.files().export(fileId=document['id'],mimeType=document['mimeType']).execute()
+        with open(document['filename'], 'wb') as f:
+            f.write(contents)
+    except HttpError as err:
+        print(err.content)
