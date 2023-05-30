20dded351a0081f19b2182e9110171f439830958
John Mertic <jmertic@linuxfoundation.org>
2023-05-30 09:08:42 -0400

    No longer use Google Auth flow
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- overview_deck/README.md
+++ overview_deck/README.md
@@ -1,7 +1,3 @@
-# Open Mainframe Project overview decks
+# Open Mainframe Project overview deck
 
-This directory contains an several of the overview decks for the Open Mainframe Project and it's hosted projects. These are updated regularly and can be used for internal presentations or in community presentations, provided that you follow the [branding guidelines](../BRANDING_GUIDELINES.md).
-
-## Decks available
-
-- "Open Source is the way forward for mainframe" that provides an overview of Open Mainframe Project in [PDF](open_mainframe_project_overview_deck.pdf) and [PPTX](open_mainframe_project_overview_deck.pptx) formats.
+This directory contains the overview deck for the Open Mainframe Project. These are updated regularly and can be used for internal presentations or in community presentations, provided that you follow the [branding guidelines](../BRANDING_GUIDELINES.md).
--- overview_deck/build.py
+++ overview_deck/build.py
@@ -7,40 +7,14 @@
 
 from __future__ import print_function
 import os.path
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 import requests
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
 # The ID of a sample document.
 documents = [
         {'id':'1Zk3REYGnE6mYIXl52QxzNbVW7XM2NPs_1w7YxscRXrk','mimeType':'application/pdf','filename':'open_mainframe_project_overview_deck.pdf'},
         {'id':'1Zk3REYGnE6mYIXl52QxzNbVW7XM2NPs_1w7YxscRXrk','mimeType':'application/vnd.openxmlformats-officedocument.presentationml.presentation','filename':'open_mainframe_project_overview_deck.pptx'},
-#        {'id':'1KaPgj8AKudkfS7t2TvOxvwWpOb5uu-GP3YwA14su_vY','mimeType':'application/pdf','filename':'open_mainframe_project_host_project.pdf'},
- #       {'id':'1KaPgj8AKudkfS7t2TvOxvwWpOb5uu-GP3YwA14su_vY','mimeType':'application/vnd.openxmlformats-officedocument.presentationml.presentation','filename':'open_mainframe_project_host_project.pptx'},
         {'id':'1SW-JbXTQgwb_OczGeyo5jqc6GmK5CLmBTzg0pb0vMXs','mimeType':'application/pdf','filename':'Open Mainframe Project - Membership Overview.pdf'},
         ]
-
-creds = None
-# The file token.json stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
 
 # Retrieve the documents contents from the Docs service.
 for document in documents:
